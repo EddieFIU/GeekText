f047e4bff13d66e3df08f411913ca55232cbe04f
amedi101 <amedi101@fiu.edu>
2022-09-24 22:37:44 -0400

    Add book browsing controller and helper class.

---
+++ GeekAPI/Controllers/Alex/BBC_Helper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GeekAPI.Controllers.Alex
+{
+    public static class BBC_Helper
+    {
+        public static JsonResult GetDbData(string connectionString, string queryScript)
+        {
+            DataTable booksTable = new();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(queryScript, conn))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    booksTable.Load(reader);
+                    reader.Close();
+                    conn.Close();
+                }
+            }
+
+            return new JsonResult(booksTable);
+        }
+
+    }
+}
---
+++ GeekAPI/Controllers/Alex/BookBrowsingController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeekAPI.Controllers.Alex
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookBrowsingController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _geekDbConnectionString;
+
+        public BookBrowsingController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _geekDbConnectionString = _configuration.GetConnectionString("GeekDBConnectionString");
+        }
+
+        // GET: api/<BookBrowsingController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            int topNum = 100;
+            string sqlQuery = $@"
+USE [GeekStore]
+
+SELECT TOP ({topNum}) [ISBN]
+      ,[Title]
+      ,[Description]
+      ,[Price]
+      ,[Genre]
+      ,[Publisher]
+      ,[YearPublished]
+      ,[CopiesSold]
+      ,[RatingID]
+      ,[AuthorID]
+  FROM [GeekStore].[dbo].[Books]";
+
+            return BBC_Helper.GetDbData(_geekDbConnectionString, sqlQuery);
+        }
+    }
+}
