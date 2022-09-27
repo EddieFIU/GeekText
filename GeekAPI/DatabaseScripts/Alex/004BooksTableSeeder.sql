9d12e30c1f077136d540594fee6885fddba99561
amedi101 <amedi101@fiu.edu>
2022-09-26 21:38:41 -0400

    Update books table seeder

--- GeekAPI/DatabaseScripts/Alex/004BooksTableSeeder.sql
+++ GeekAPI/DatabaseScripts/Alex/004BooksTableSeeder.sql
@@ -1,4 +1,4 @@
 ﻿USE [GeekStore]
 
 INSERT INTO [dbo].[Books] (BookID, ISBN, Title, Description, Price, Genre, Publisher, YearPublished, CopiesSold)
-VALUES (1, 1111111111111, 'Diary of a Wimpy Kid', 'Greg Heffley', 9.99, 'Horror', 'Amulet', 2007, 1);
+VALUES (1, 	9780143303831, 'Diary of a Wimpy Kid', 'It’s a new school year, and Greg Heffley finds himself thrust into middle school, where undersized weaklings share the hallways with kids who are taller, meaner, and already shaving. The hazards of growing up before you’re ready are uniquely revealed through words and drawings as Greg records them in his diary.', 14.99, 'Comedy', 'Amulet Books', 2007, 20000000);
