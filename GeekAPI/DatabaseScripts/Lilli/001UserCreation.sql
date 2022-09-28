e8d50c7129eb092a6a889f7878076fbe8e8c60a9
lmila011 <lmila011@fiu.edu>
2022-09-28 10:26:53 -0400

    Created User table and Credit Card Table.

---
+++ GeekAPI/DatabaseScripts/Lilli/001UserCreation.sql
@@ -0,0 +1,13 @@
+﻿USE [GeekStore]
+GO
+
+CREATE TABLE [dbo].[USERS](
+    [UserID] int IDENTITY(1,1) PRIMARY KEY,
+    [Username] VARCHAR(20) NOT NULL,
+    [Password] VARCHAR(20) NOT NULL,
+    [Name] VARCHAR(max) NOT NULL,
+    [Email] VARCHAR(20) NOT NULL,
+    [HomeAddress] VARCHAR(20) NOT NULL
+  
+    )
+GO
---
+++ GeekAPI/DatabaseScripts/Lilli/002CreditCard.sql
@@ -0,0 +1,13 @@
+﻿USE [GeekStore]
+GO
+
+CREATE TABLE [dbo].[CREDITCARDS](
+    [CCID] int IDENTITY(1,1) PRIMARY KEY,
+    [CardNumber] VARCHAR(16) NOT NULL,
+    [ExpDate] DATETIME NOT NULL,
+    [PIN] INT NOT NULL,
+    [Name] VARCHAR (20) NOT NULL,
+    [ZipCode] INT NOT NULL
+  
+    )
+GO
