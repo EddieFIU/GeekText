742d15d42a95e118d28e062689078f466af372aa
lsdbf <zdspeg@gmail.com>
2022-09-28 21:48:07 -0400

    Updated shoppingcartcreation to have foreign key UserID

--- GeekAPI/DatabaseScripts/Ethan/001ShoppingCartCreation.sql
+++ GeekAPI/DatabaseScripts/Ethan/001ShoppingCartCreation.sql
@@ -3,7 +3,7 @@ GO
 
 CREATE TABLE [dbo].[ShoppingCart](
     [ShoppingCartID] int IDENTITY(1,1) PRIMARY KEY,
-    [UserID] int NOT NULL,
+    [UserID] int NOT NULL FOREIGN KEY REFERENCES dbo.USERS(UserID),
     [BookID] int NOT NULL FOREIGN KEY REFERENCES dbo.Books(BookID)
     )
 GO
--- GeekAPI/DatabaseScripts/Ethan/002ShoppingCartSeeder.sql
+++ GeekAPI/DatabaseScripts/Ethan/002ShoppingCartSeeder.sql
@@ -9,3 +9,4 @@ INSERT INTO [dbo].[ShoppingCart]
 GO
 
 
+--testing values
