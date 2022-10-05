7be955c15db5fb94b7417cd66fa00b090e634ec6
Eddie Molieri <emoli018@fiu.edu>
2022-10-05 13:46:47 -0400

    comments table

---
+++ GeekAPI/DatabaseScripts/Eddie/005AddFieldsToCommentTable.sql
@@ -0,0 +1,44 @@
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+CREATE TABLE dbo.Tmp_Comment
+	(
+	CommentID int NOT NULL,
+	UserID int NOT NULL,
+	RatingID int NOT NULL,
+	Comment varchar(2000) NOT NULL,
+	CreateDate datetime NOT NULL
+	)  ON [PRIMARY]
+GO
+ALTER TABLE dbo.Tmp_Comment SET (LOCK_ESCALATION = TABLE)
+GO
+IF EXISTS(SELECT * FROM dbo.Comment)
+	 EXEC('INSERT INTO dbo.Tmp_Comment (CommentID, UserID, Comment, CreateDate)
+		SELECT CommentID, UserID, Comment, CreateDate FROM dbo.Comment WITH (HOLDLOCK TABLOCKX)')
+GO
+DROP TABLE dbo.Comment
+GO
+EXECUTE sp_rename N'dbo.Tmp_Comment', N'Comment', 'OBJECT' 
+GO
+ALTER TABLE dbo.Comment ADD CONSTRAINT
+	PK_Comment PRIMARY KEY CLUSTERED 
+	(
+	CommentID
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX IX_Rating ON dbo.Comment
+	(
+	RatingID
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+COMMIT
