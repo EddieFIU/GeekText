645028a7f091f7c0a0b483b3a77bbdbcbce2fd37
Carlos Melgar <cmelg004@fiu.edu>
2022-10-07 09:58:03 -0400

    Book details controller

---
+++ GeekAPI/Controllers/Carlos/BookDetailsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeekAPI.Controllers.Alex
+{
+    [ApiController]
+    public class BookDetailsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _geekDbConnectionString;
+
+        public BookDetailsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _geekDbConnectionString = _configuration.GetConnectionString("GeekDBConnectionString");
+        }
+
+        [Route("api/[controller]")]
+        [Route("api/test")]
+        [HttpGet]
+        public JsonResult ExampleGet()
+        {
+            int topNum = 100;
+            string sqlQuery = $@"
+USE [GeekStore]
+
+SELECT TOP ({topNum}) [BookID]
+      ,[ISBN]
+      ,[Title]
+      ,[Description]
+      ,[Price]
+      ,[Genre]
+      ,[Publisher]
+      ,[YearPublished]
+      ,[CopiesSold]
+      ,[RatingID]
+      ,[AuthorID]
+  FROM [GeekStore].[dbo].[Books]";
+
+            return Alex.SQL_Helper.GetDbData(_geekDbConnectionString, sqlQuery);
+        }
+
+    }
+}
