708e8d8824d59e70b8d23c18f939fe0d4c8a7a8d
Carlos Melgar <cmelg004@fiu.edu>
2022-10-10 07:49:47 -0400

    Update to authors table

---
+++ GeekAPI/DatabaseScripts/Carlos/001AuthorsTableSeeder.sql
@@ -0,0 +1,15 @@
+﻿USE [GeekStore]
+
+INSERT INTO [dbo].[Authors] (AuthorID, FirstName, LastName, Biography, Publisher)
+
+VALUES
+
+('Wimpy', 'Jeff', 'Kinney', 'Diary of a Wimpy Kid author Jeff Kinney didn’t grow up wanting to be a children’s author. His dream was to become a newspaper cartoonist but he wasn’t able to get his comic strip syndicated.', 'ABRAMS'),
+
+('J.K. Rowling', 'Joanne', 'Rowling', 'British writer J.K. Rowling is the creator of the character Harry Potter. Her books and movies about the boy wizard are popular with children and adults throughout the world. Joanne Rowling was born on July 31, 1965, in Yate, England. She first thought up Harry Potter in 1990 during a long train delay.', 'Bloomsbury'),
+
+('J.R.R. Tolkien', 'John', 'Tolkien', 'J.R.R. Tolkien, in full John Ronald Reuel Tolkien, (born January 3, 1892, Bloemfontein, South Africa—died September 2, 1973, Bournemouth, Hampshire, England), English writer and scholar who achieved fame with his children’s book The Hobbit (1937) and his richly inventive epic fantasy The Lord of the Rings (1954–55).', 'George Allen & Unwin'),
+
+('C.S. Lewis', 'Clive', 'Lewis', 'C.S. Lewis, in full Clive Staples Lewis, (born November 29, 1898, Belfast, Ireland [now in Northern Ireland]—died November 22, 1963, Oxford, Oxfordshire, England), Irish-born scholar, novelist, and author of about 40 books, many of them on Christian apologetics, including The Screwtape Letters and Mere Christianity. His works of greatest lasting fame may be The Chronicles of Narnia, a series of seven children’s books that have become classics of fantasy literature.', 'Geoffry Bles'),
+
+('Suzanne Collins', 'Suzanne', 'Collins', 'Suzanne Collins, (born August 10, 1962, Hartford, Connecticut, U.S.), American author and screenwriter, best known for the immensely popular Hunger Games series of young-adult novels.', 'Scholastic');
