e441e4498db3525ec0b0c9660d9f8169239d08a0
Carlos Melgar <cmelg004@fiu.edu>
2022-10-31 08:34:34 -0400

    Updating the search function

--- GeekAPI/Controllers/Carlos/SearchFuncionController.cs
+++ GeekAPI/Controllers/Carlos/SearchFuncionController.cs
@@ -2,7 +2,29 @@
 
 namespace GeekAPI.Controllers.Carlos
 {
-    public class SearchFuncionController : ControllerBase
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchFunctionController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+        private readonly string _geekDbConnectionString;
+
+        public SearchFunctionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _geekDbConnectionString = _configuration.GetConnectionString("GeekDBConnectionString");
+        }
+        //GET book name
+        [HttpGet("{name}")]
+        public JsonResult GetName(string name)
+        {
+            string sqlQuery = $@"
+            USE [GeekStore]
+    
+            SELECT *
+            from [dbo].[Books] 
+            where BookName = " + name;
+            return Alex.SQL_Helper.GetDbData(_geekDbConnectionString, sqlQuery);
+        }
     }
 }
